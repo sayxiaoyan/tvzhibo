6189b631b23a51ac9c27ef7aed5ecf6deaf3e802
sayxiaoyan <127412316+sayxiaoyan@users.noreply.github.com>
2024-02-25 23:15:12 +0800

    Update qita.py

--- qita.py
+++ qita.py
@@ -104,7 +104,7 @@ result_counter = 8  # 每个频道需要的个数
 
 with open("qita.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
-    file.write('其他频道,#genre#\n')
+    file.write('地方频道,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
         if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name:
@@ -122,7 +122,7 @@ with open("qita.txt", 'w', encoding='utf-8') as file:
 
 with open("qita.m3u", 'w', encoding='utf-8') as file:
     channel_counters = {}
-    #file.write('其他频道,#genre#\n')
+    #file.write('地方频道,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
         if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name:
@@ -130,11 +130,11 @@ with open("qita.m3u", 'w', encoding='utf-8') as file:
                 if channel_counters[channel_name] >= result_counter:
                     continue
                 else:
-                    file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
+                    file.write(f"#EXTINF:-1 group-title=\"地方频道\",{channel_name}\n")
                     file.write(f"{channel_url}\n")
                     channel_counters[channel_name] += 1
             else:
-                file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
+                file.write(f"#EXTINF:-1 group-title=\"地方频道\",{channel_name}\n")
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1
     
